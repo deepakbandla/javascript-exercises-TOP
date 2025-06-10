a4f6d62aa92eb928799186e3191b1144bc7e2445
Deepak Bandla <deepakbandla4567@gmail.com>
2025-06-10 06:31:22 +0000

    Fix reverseString function implementation to accept a word parameter and return its reversed version

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let f = "";
+    for(let i = word.length-1; i>=0; i--){
+        f+=word[i];
+    }
+    return f;
 };
 
 // Do not edit below this line
